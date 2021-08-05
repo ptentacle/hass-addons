204a6cbdf6ed876acc9d9d75e7d5d726d57dd3b3
Alex <wilcowilcox@gmail.com>
2021-08-05 22:48:57 +0100

    Initial

---
+++ cloudflareddns/data/hooks.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# shellcheck disable=SC2034
+set -e
+
+CONFIG_PATH=/data/options.json
+
+CLOUDFLARE_AUTH_KEY=$(jq --raw-output '.token' $CONFIG_PATH)
+ZONE_ID=$(jq --raw-output '.zone_id' $CONFIG_PATH)
+SYS_CERTFILE=$(jq --raw-output '.lets_encrypt.certfile' $CONFIG_PATH)
+SYS_KEYFILE=$(jq --raw-output '.lets_encrypt.keyfile' $CONFIG_PATH)
+
+
+# https://github.com/lukas2511/dehydrated/blob/master/docs/examples/hook.sh
+
+deploy_challenge() {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}" ALIAS
+    ALIAS="$(jq --raw-output --exit-status "[.aliases[]|{(.domain):.alias}]|add.\"$DOMAIN\"" $CONFIG_PATH)" || ALIAS="$DOMAIN"
+
+    # This hook is called once for every domain that needs to be
+    # validated, including any alternative names you may have listed.
+
+    # get the dns TXT record id
+    DNSRECORDID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=TXT&name=$ALIAS" \
+        -H "Authorization: Bearer $CLOUDFLARE_AUTH_KEY" \
+        -H "Content-Type: application/json" | jq -r  '{"result"}[] | .[0] | .id')
+
+    # update TXT record
+    SUCCESS=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNSRECORDID" \
+        -H "Authorization: Bearer $CLOUDFLARE_AUTH_KEY" \
+        -H "Content-Type: application/json" \
+        --data "{\"type\":\"TXT\",\"name\":\"$ALIAS\",\"content\":\"$TOKEN_VALUE\",\"ttl\":1,\"proxied\":false}" | jq -r  {"success"}[])
+
+}
+
+clean_challenge() {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}" ALIAS
+    ALIAS="$(jq --raw-output --exit-status "[.aliases[]|{(.domain):.alias}]|add.\"$DOMAIN\"" $CONFIG_PATH)" || ALIAS="$DOMAIN"
+
+    # This hook is called after attempting to validate each domain,
+    # whether or not validation was successful. Here you can delete
+    # files or DNS records that are no longer needed.
+    #
+    # The parameters are the same as for deploy_challenge.
+
+    # get the dns TXT record id
+    DNSRECORDID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=TXT&name=$ALIAS" \
+        -H "Authorization: Bearer $CLOUDFLARE_AUTH_KEY" \
+        -H "Content-Type: application/json" | jq -r  '{"result"}[] | .[0] | .id')
+
+    # update TXT record
+    SUCCESS=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNSRECORDID" \
+        -H "Authorization: Bearer $CLOUDFLARE_AUTH_KEY" \
+        -H "Content-Type: application/json" \
+        --data "{\"type\":\"TXT\",\"name\":\"$ALIAS\",\"content\":\"removed\",\"ttl\":1,\"proxied\":false}" | jq -r  {"success"}[])
+
+}
+
+deploy_cert() {
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}" TIMESTAMP="${6}"
+
+    # This hook is called once for each certificate that has been
+    # produced. Here you might, for instance, copy your new certificates
+    # to service-specific locations and reload the service.
+    #
+    # Parameters:
+    # - DOMAIN
+    #   The primary domain name, i.e. the certificate common
+    #   name (CN).
+    # - KEYFILE
+    #   The path of the file containing the private key.
+    # - CERTFILE
+    #   The path of the file containing the signed certificate.
+    # - FULLCHAINFILE
+    #   The path of the file containing the full certificate chain.
+    # - CHAINFILE
+    #   The path of the file containing the intermediate certificate(s).
+    # - TIMESTAMP
+    #   Timestamp when the specified certificate was created.
+
+     cp -f "$FULLCHAINFILE" "/ssl/$SYS_CERTFILE"
+     cp -f "$KEYFILE" "/ssl/$SYS_KEYFILE"
+}
+
+
+HANDLER="$1"; shift
+if [[ "${HANDLER}" =~ ^(deploy_challenge|clean_challenge|deploy_cert)$ ]]; then
+  "$HANDLER" "$@"
+fi
---
+++ cloudflareddns/data/run.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bashio
+
+CERT_DIR=/data/letsencrypt
+WORK_DIR=/data/workdir
+
+# Let's encrypt
+LE_UPDATE="0"
+
+# DuckDNS
+if bashio::config.has_value "ipv4"; then IPV4=$(bashio::config 'ipv4'); else IPV4=""; fi
+if bashio::config.has_value "ipv6"; then IPV6=$(bashio::config 'ipv6'); else IPV6=""; fi
+CLOUDFLARE_AUTH_KEY=$(bashio::config 'token')
+ZONE_ID=$(bashio::config 'zone_id')
+DOMAINS=$(bashio::config 'domains | join(",")')
+WAIT_TIME=$(bashio::config 'seconds')
+
+# Function that performe a renew
+function le_renew() {
+    local domain_args=()
+    local domains=''
+    local aliases=''
+
+    domains=$(bashio::config 'domains')
+
+    # Prepare domain for Let's Encrypt
+    for domain in ${domains}; do
+        for alias in $(jq --raw-output --exit-status "[.aliases[]|{(.alias):.domain}]|add.\"${domain}\" | select(. != null)" /data/options.json) ; do
+            aliases="${aliases} ${alias}"
+        done
+    done
+
+    aliases="$(echo "${aliases}" | tr ' ' '\n' | sort | uniq)"
+
+    bashio::log.info "Renew certificate for domains: $(echo -n "${domains}") and aliases: $(echo -n "${aliases}")"
+
+    for domain in $(echo "${domains}" "${aliases}" | tr ' ' '\n' | sort | uniq); do
+        domain_args+=("--domain" "${domain}")
+    done
+
+    dehydrated --cron --hook ./hooks.sh --challenge dns-01 "${domain_args[@]}" --out "${CERT_DIR}" --config "${WORK_DIR}/config" || true
+    LE_UPDATE="$(date +%s)"
+}
+
+# Register/generate certificate if terms accepted
+if bashio::config.true 'lets_encrypt.accept_terms'; then
+    # Init folder structs
+    mkdir -p "${CERT_DIR}"
+    mkdir -p "${WORK_DIR}"
+
+    # Clean up possible stale lock file
+    if [ -e "${WORK_DIR}/lock" ]; then
+        rm -f "${WORK_DIR}/lock"
+        bashio::log.warning "Reset dehydrated lock file"
+    fi
+
+    # Generate new certs
+    if [ ! -d "${CERT_DIR}/live" ]; then
+        # Create empty dehydrated config file so that this dir will be used for storage
+        touch "${WORK_DIR}/config"
+
+        dehydrated --register --accept-terms --config "${WORK_DIR}/config"
+    fi
+fi
+
+# Run duckdns
+while true; do
+
+    [[ ${IPV4} != *:/* ]] && ipv4=${IPV4} || ipv4=$(curl -s -m 10 "${IPV4}")
+    [[ ${IPV6} != *:/* ]] && ipv6=${IPV6} || ipv6=$(curl -s -m 10 "${IPV6}")
+
+    # IPv4
+    domains=$(bashio::config 'domains')
+    for domain in ${domains}; do
+        # get the dns A record id
+        dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?type=A&name=${dnsrecord}" \
+        -H "Authorization: Bearer ${CLOUDFLARE_AUTH_KEY}" \
+        -H "Content-Type: application/json" | jq -r  '{"result"}[] | .[0] | .id')
+
+        # update A record
+        answer=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${dnsrecordid}" \
+        -H "Authorization: Bearer ${CLOUDFLARE_AUTH_KEY}" \
+        -H "Content-Type: application/json" \
+        --data "{\"type\":\"A\",\"name\":\"${domain}\",\"content\":\"${IPV4}\",\"ttl\":1,\"proxied\":false}" | jq -r  {"success"}[])
+
+        if [ "${answer}" == "true" ]; then
+            bashio::log.info "Update A record success"
+        else
+            bashio::log.warning "Update A record failed"
+        fi
+    done
+    
+    now="$(date +%s)"
+    if bashio::config.true 'lets_encrypt.accept_terms' && [ $((now - LE_UPDATE)) -ge 43200 ]; then
+        le_renew
+    fi
+    
+    sleep "${WAIT_TIME}"
+done
